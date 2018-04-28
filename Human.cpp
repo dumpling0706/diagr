29c8ec798f5875119c9a8b713f3ce25185f87391
dumpling0706 <38697158+dumpling0706@users.noreply.github.com>
2018-04-28 14:01:22 +0900

    Update Human.cpp

--- Human.cpp
+++ Human.cpp
@@ -8,35 +8,35 @@ Human::Human()
 
 }
 
-Human::Human(string patronymic, string name, string second_name)
+Human::Human(string patronymic, string name, string second_name)//задает отчество,имя,фамилию
 {
 	setPatronymic(patronymic);
 	setName(name);
 	setSName( second_name);
 }
 
-void Human::setName(string name)
+void Human::setName(string name)//установка имени,передаваемым параметром name
 {
-	name_ = name;
+	name_ = name;//помещаем имя в переменную name_
 }
 
-void Human::setPatronymic(string patronymic)
+void Human::setPatronymic(string patronymic)//установка отчества
 {
 	patronymic_ = patronymic;
 }
 
-void Human::setSName(string sec_name)
+void Human::setSName(string sec_name)//установка фамилии 
 {
 	second_name_ = sec_name;
 }
 
 
-string Human::fullName()
+string Human::fullName()//возвращает строку(полное имя)
 {
 	return name_+" "+second_name_+" "+ patronymic_;
 }
 
 
-Human::~Human()
+Human::~Human()//деструктор
 {
 }
